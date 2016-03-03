eb70e8bb0d210447a3be099f018b40dc8af0c04e
Petr Sloup <slouppetr@gmail.com>
2016-03-03 11:17:38 +0100

    Add basic command line options

--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@
 
 'use strict';
 
+var opts = require('nomnom')
+  .option('config', {
+    abbr: 'c',
+    default: 'config.json',
+    help: 'Configuration file'
+  })
+  .option('port', {
+    abbr: 'p',
+    default: 8080,
+    help: 'Port'
+  })
+  .option('version', {
+    abbr: 'v',
+    flag: true,
+    help: 'Version info',
+    callback: function() {
+      return 'version ' + require('../package.json').version;
+    }
+  }).parse();
+
 return require('./server')({
-  config: 'config.json',
-  port: 8080
+  config: opts.config,
+  port: opts.port
 });
