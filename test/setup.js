832b2d22bee2406607d03062b51f2cc5e039360c
Petr Sloup <slouppetr@gmail.com>
2016-03-09 12:34:16 +0100

    Do not invoke `done` in test setup

--- test/setup.js
+++ test/setup.js
@@ -3,13 +3,13 @@ process.env.NODE_ENV = 'test';
 global.should = require('should');
 global.supertest = require('supertest');
 
-before(function(done) {
+before(function() {
   console.log('global setup');
   process.chdir('test_data');
   var running = require('../src/server')({
     config: 'config.json',
     port: 8888
-  }, done);
+  });
   global.app = running.app;
   global.server = running.server;
 });
