b98b7244f63fada83a98662f5723c5b1082d2053
Petr Sloup <slouppetr@gmail.com>
2016-03-11 09:16:28 +0100

    Correctly serve sprites

--- src/serve_style.js
+++ src/serve_style.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+    fs = require('fs');
 
 var clone = require('clone'),
     express = require('express');
@@ -23,8 +24,11 @@ module.exports = function(repo, options, id, reportVector) {
       source.url = 'local://vector/' + identifier + '.json';
     }
   });
-  styleJSON.sprite = 'local://styles/{style_id}/sprite';
-  styleJSON.glyphs = 'local://fonts/{fonstack}/{range}.pbf';
+
+  var spritePath = path.join(rootPath, styleJSON.sprite);
+
+  styleJSON.sprite = 'local://styles/' + id + '/sprite';
+  styleJSON.glyphs = 'local://fonts/{fontstack}/{range}.pbf';
 
   repo[id] = styleJSON;
 
@@ -42,6 +46,23 @@ module.exports = function(repo, options, id, reportVector) {
     styleJSON_.sprite = fixUrl(styleJSON_.sprite);
     styleJSON_.glyphs = fixUrl(styleJSON_.glyphs);
     return res.send(styleJSON_);
+  });
+
+  app.get('/styles/' + id + '/sprite:scale(@[23]x)?\.:format([\\w]+)',
+      function(req, res, next) {
+    var scale = req.params.scale,
+        format = req.params.format;
+    var filename = spritePath + (scale || '') + '.' + format;
+    return fs.readFile(filename, function(err, data) {
+      if (err) {
+        console.log('Sprite load error:', filename);
+        return res.status(404).send('File not found');
+      } else {
+        if (format == 'json') res.header('Content-type', 'application/json');
+        if (format == 'png') res.header('Content-type', 'image/png');
+        return res.send(data);
+      }
+    });
   });
 
   return app;
