801c523c73b95bfaf9d8e20c4adf585ac1001c98
Petr Sloup <slouppetr@gmail.com>
2016-05-30 22:22:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xvfb-run --server-args="-screen 0 1024x768x24" node /usr/src/app/src/main.js -p 80 -c /data/config.json
+xvfb-run -a -e /dev/stdout --server-args="-screen 0 1024x768x24" node /usr/src/app/src/main.js -p 80 -c /data/config.json
