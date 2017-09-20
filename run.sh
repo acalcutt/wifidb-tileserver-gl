da646868c3d610a8612e8302dc391502d077d3bb
Petr Sloup <slouppetr@gmail.com>
2017-09-20 08:05:10 +0200

    Wait longer for xvfb to start up (#197)

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
-sleep 1
+echo "Waiting 3 seconds for xvfb to start..."
+sleep 3
 
 export DISPLAY=:99.0
 
