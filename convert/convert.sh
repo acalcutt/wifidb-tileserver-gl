03d0b5451bb3cecbbc3b1da745cb629ede85de2a
acalcutt <acalcutt@techidiots.net>
2020-07-27 20:13:30 -0400

    Update convert.sh

--- convert/convert.sh
+++ convert/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Input_Folder='/mnt/nvme/convert/in'
-Output_Folder='/mnt/nvme/convert/out'
-Tmp_Folder='/mnt/nvme/tmp'
+Input_Folder='/opt/convert/in'
+Output_Folder='/opt/convert/out'
+Tmp_Folder='/tmp'
 TileServer_Data='/opt/tileserver-gl/data'
 tippecanoe_bin='/opt/tippecanoe-1.35.0'
 WifiDB_BaseURL='https://wifidb.net/wifidb/out/geojson'
@@ -29,9 +29,9 @@ wget -O $Input_Folder/WifiDB_Legacy.json $WifiDB_BaseURL/WifiDB_Legacy.json
 wget -O $Input_Folder/cell_networks.json $WifiDB_BaseURL/cell_networks.json
 
 echo "---Convert Geojson to mbtiles---"
-$tippecanoe_bin/tippecanoe -z18 -t $Tmp_Folder -M 750000 -O 500000 -o /mnt/nvme/convert/out/cell_networks.mbtiles -n "WifiDB_cell $(date -d "today" +"%Y-%m-%d %H:%M")" -A "<a href=""https://wifidb.net/"" target=""_blank"">&copy; WifiDB $(date -d "today" +"%Y-%m-%d")</a>" --drop-densest-as-needed /mnt/nvme/convert/in/cell_networks.json
-$tippecanoe_bin/tippecanoe -z18 -t $Tmp_Folder -M 750000 -O 500000 -o /mnt/nvme/convert/out/WifiDB_newest.mbtiles -n "WifiDB_newest $(date -d "today" +"%Y-%m-%d %H:%M")" -A "<a href=""https://wifidb.net/"" target=""_blank"">&copy; WifiDB $(date -d "today" +"%Y-%m-%d")</a>" --drop-densest-as-needed /mnt/nvme/convert/in/WifiDB_0to1year.json /mnt/nvme/convert/in/WifiDB_monthly.json /mnt/nvme/convert/in/WifiDB_weekly.json
-$tippecanoe_bin/tippecanoe -z18 -t $Tmp_Folder -M 750000 -O 500000 -o /mnt/nvme/convert/out/WifiDB.mbtiles -n "WifiDB $(date -d "today" +"%Y-%m-%d %H:%M")" -A "<a href=""https://wifidb.net/"" target=""_blank"">&copy; WifiDB $(date -d "today" +"%Y-%m-%d")</a>" --drop-densest-as-needed /mnt/nvme/convert/in/WifiDB_Legacy.json /mnt/nvme/convert/in/WifiDB_2to3year.json /mnt/nvme/convert/in/WifiDB_1to2year.json
+$tippecanoe_bin/tippecanoe -z18 -t $Tmp_Folder -M 750000 -O 500000 -o $Output_Folder/cell_networks.mbtiles -n "WifiDB_cell $(date -d "today" +"%Y-%m-%d %H:%M")" -A "<a href=""https://wifidb.net/"" target=""_blank"">&copy; WifiDB $(date -d "today" +"%Y-%m-%d")</a>" --drop-densest-as-needed $Input_Folder/cell_networks.json
+$tippecanoe_bin/tippecanoe -z18 -t $Tmp_Folder -M 750000 -O 500000 -o $Output_Folder/WifiDB_newest.mbtiles -n "WifiDB_newest $(date -d "today" +"%Y-%m-%d %H:%M")" -A "<a href=""https://wifidb.net/"" target=""_blank"">&copy; WifiDB $(date -d "today" +"%Y-%m-%d")</a>" --drop-densest-as-needed $Input_Folder/WifiDB_0to1year.json $Input_Folder/WifiDB_monthly.json $Input_Folder/WifiDB_weekly.json
+$tippecanoe_bin/tippecanoe -z18 -t $Tmp_Folder -M 750000 -O 500000 -o $Output_Folder/WifiDB.mbtiles -n "WifiDB $(date -d "today" +"%Y-%m-%d %H:%M")" -A "<a href=""https://wifidb.net/"" target=""_blank"">&copy; WifiDB $(date -d "today" +"%Y-%m-%d")</a>" --drop-densest-as-needed $Input_Folder/WifiDB_Legacy.json $Input_Folder/WifiDB_2to3year.json $Input_Folder/WifiDB_1to2year.json
 
 echo "---Move file and restart tileserver gl---"
 mv $Output_Folder/cell_networks.mbtiles $TileServer_Data
