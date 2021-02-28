0a1987f7f5e3e3cf4dc5c32cd3ad44d03f635929
acalcutt <acalcutt@techidiots.net>
2021-02-27 19:34:57 -0500

    Update create_mbtiles.sh

--- osm_basemap/create_mbtiles.sh
+++ osm_basemap/create_mbtiles.sh
@@ -1,6 +1,19 @@
 #https://osmdata.openstreetmap.de/
-#ogr2ogr -f GeoJSON water.geojson water-polygons-split-4326/water_polygons.shp
-#ogr2ogr -f GeoJSON land.geojson land-polygons-complete-4326/land_polygons.shp
-#ogr2ogr -f GeoJSON coastlines.geojson coastlines-split-4326/lines.shp
+#ogr2ogr -f GeoJSON water.geojson data/water-polygons-split-4326/water_polygons.shp
+#ogr2ogr -f GeoJSON land.geojson data/land-polygons-complete-4326/land_polygons.shp
+#ogr2ogr -f GeoJSON coastlines.geojson data/coastlines-split-4326/lines.shp
 
-/opt/tippecanoe-1.35.0/tippecanoe -zg -o /opt/convert/osm_base.mbtiles --drop-densest-as-needed --extend-zooms-if-still-dropping land.geojson water.geojson coastlines.geojson ne_10m_lakes.geojson ne_10m_rivers_lake_centerlines.geojson ne_10m_admin_1_states_provinces.geojson
+[ ! -f data/water-polygons-split-4326.zip ] && wget https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip -P data
+unzip -o data/water-polygons-split-4326.zip -d data
+
+[ ! -f data/land-polygons-complete-4326.zip ] && wget https://osmdata.openstreetmap.de/download/land-polygons-complete-4326.zip -P data
+unzip -o data/land-polygons-complete-4326.zip -d data
+
+[ ! -f data/coastlines-split-4326.zip ] && wget https://osmdata.openstreetmap.de/download/coastlines-split-4326.zip -P data
+unzip -o data/coastlines-split-4326.zip -d data
+
+[ ! -f data/water.geojson ] && ogr2ogr -progress -f GeoJSON data/water.geojson data/water-polygons-split-4326/water_polygons.shp
+[ ! -f data/land.geojson ] && ogr2ogr -progress -f GeoJSON data/land.geojson data/land-polygons-complete-4326/land_polygons.shp
+[ ! -f data/coastlines.geojson ] && ogr2ogr -progress -f GeoJSON data/coastlines.geojson data/coastlines-split-4326/lines.shp
+
+/opt/tippecanoe-1.35.0/tippecanoe -zg -o data/osm_basemap.mbtiles --drop-densest-as-needed --extend-zooms-if-still-dropping data/land.geojson data/water.geojson data/coastlines.geojson
