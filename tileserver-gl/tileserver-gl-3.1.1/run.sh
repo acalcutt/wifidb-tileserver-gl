74b7aba4d09c9e3841499f2883ac572e3fd7884a
acalcutt <acalcutt@techidiots.net>
2021-09-29 22:36:15 -0400

    current tileserver/config/styles

--- tileserver-gl/tileserver-gl-3.1.1/run.sh
+++ tileserver-gl/tileserver-gl-3.1.1/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install v10.21.0
-
 _term() {
 	echo "Caught signal, stopping gracefully"
 	kill -TERM "$child" 2>/dev/null
@@ -12,7 +8,7 @@ _term() {
 trap _term SIGTERM
 trap _term SIGINT
 
-xvfbMaxStartWaitTime=5
+xvfbMaxStartWaitTime=60
 displayNumber=99
 screenNumber=0
 
@@ -22,7 +18,7 @@ rm -rf /tmp/.X11-unix /tmp/.X${displayNumber}-lock ~/xvfb.pid
 echo "Starting Xvfb on display ${displayNumber}"
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :${displayNumber} -screen ${screenNumber} 1024x768x24  -ac +extension GLX +render -noreset
 
-# Wait to be able to connect to the port. This will exit if it cannot in 15 minutes.
+# Wait to be able to connect to the port. This will exit if it cannot in 1 minute.
 timeout ${xvfbMaxStartWaitTime} bash -c "while  ! xdpyinfo -display :${displayNumber} >/dev/null; do sleep 0.5; done"
 if [ $? -ne 0 ]; then
   echo "Could not connect to display ${displayNumber} in ${xvfbMaxStartWaitTime} seconds time."
@@ -31,9 +27,9 @@ fi
 
 export DISPLAY=:${displayNumber}.${screenNumber}
 
-
-nvm use v10.21.0
-node /opt/tileserver-gl/tileserver-gl-3.0.0/ -p 8080 -c /opt/tileserver-gl/configs/config-3.0.0.json  "$@" & 
+echo
+cd /data
+node /usr/src/app/ -p 80 "$@" &
 child=$!
 wait "$child"
 
